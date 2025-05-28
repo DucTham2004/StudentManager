acf1368062f30ea4dcedb752b8d97361be274faf
BURNITHUST <ductham2004@gmail.com>
2025-05-28 21:53:02 +0700

    use SQLite to save data

--- app/src/main/java/com/example/studentmanager/MainActivity.kt
+++ app/src/main/java/com/example/studentmanager/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.studentmanager
 
 import android.app.Activity
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
 import android.os.Bundle
 import android.view.ContextMenu
@@ -26,13 +27,33 @@ class MainActivity : AppCompatActivity() {
     lateinit var adapter: StudentAdapter
     lateinit var intentAdd: Intent
     lateinit var intentUpdate: Intent
+    lateinit var db: SQLiteDatabase
+    val tableName = "tbStudent"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         students = mutableListOf<StudentModel>()
-        students.add(StudentModel("Nguyen Duc Tham", "20225395", "ductham2004@gmail.com", "0393036465"))
-        students.add(StudentModel("Nguyen Van A", "20225495", "abc@gmail.com", "0393436465"))
-        students.add(StudentModel("Nguyen Thi B", "20225355", "xvd@gmail.com", "0334325465"))
+        db = SQLiteDatabase.openDatabase(filesDir.path +"/studentdb",null,SQLiteDatabase.CREATE_IF_NECESSARY)
+        if(!isTableExists(db, tableName)) // neu chua ton tai bang
+        {
+            // tao bang
+            createTable(tableName, db)
+        }
+        val cursor = db.rawQuery(
+            "select *" +
+                    " from $tableName ", null
+        )
+
+        while (cursor.moveToNext()){
+            if(cursor.getInt(4) != 0)  // =0 la da xoa
+            {
+                students.add(StudentModel(cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3)))
+            }
+        }
+
+        cursor.close()
 
         val listViewStudent = findViewById<ListView>(R.id.list_student)
         adapter = StudentAdapter(students)
@@ -48,6 +69,7 @@ class MainActivity : AppCompatActivity() {
                 val studentMSSV = it.data?.getStringExtra("studentMSSV")
                 val studentEmail = it.data?.getStringExtra("studentEmail")
                 val studentPhoneNumber = it.data?.getStringExtra("studentPhoneNumber")
+                addDataNewStudent(studentName,studentMSSV,studentEmail,studentPhoneNumber)
                 students.add(StudentModel(studentName.toString(),studentMSSV.toString(),studentEmail.toString(),studentPhoneNumber.toString()))
                 adapter.notifyDataSetChanged()
             }
@@ -63,6 +85,7 @@ class MainActivity : AppCompatActivity() {
                 val studentUpdateEmail = it.data?.getStringExtra("studentUpdateEmail")
                 val studentUpdatePhoneNumber = it.data?.getStringExtra("studentUpdatePhoneNumber")
                 val position = it.data?.getIntExtra("position",0)
+                updateDataStudent(position,studentUpdateName,studentUpdateMSSV,studentUpdateEmail,studentUpdatePhoneNumber  )
                 students[position!!].name = studentUpdateName.toString()
                 students[position].mssv = studentUpdateMSSV.toString()
                 students[position].email = studentUpdateEmail.toString()
@@ -114,6 +137,7 @@ class MainActivity : AppCompatActivity() {
                 updateStudentLauncher.launch(intentUpdate)
             }
             R.id.itemDelete -> {
+                deleteDataStudent(students[info.position].mssv)
                 students.removeAt(info.position)
                 adapter.notifyDataSetChanged()
 
@@ -134,4 +158,76 @@ class MainActivity : AppCompatActivity() {
 
         return true
     }
+
+
+    fun createTable(table: String, db: SQLiteDatabase) {
+        db.beginTransaction()
+        try {
+            db.execSQL("create table $table(" +
+                    "name text ," +
+                    "mssv text," +
+                    "email text," +
+                    "phone text," +
+                    "status integer)")
+            db.setTransactionSuccessful()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        } finally {
+            db.endTransaction()
+        }
+    }
+
+    fun addDataNewStudent(studentName: String?, studentMSSV: String?, studentEmail: String?, studentPhoneNumber: String?){
+        db.beginTransaction()
+        try {
+            db.execSQL("insert into $tableName values(?,?,?,?,1)", arrayOf(studentName ?:"",studentMSSV ?: "",studentEmail ?:"",studentPhoneNumber ?: ""))
+            db.setTransactionSuccessful()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        } finally {
+            db.endTransaction()
+        }
+    }
+
+    fun deleteDataStudent(mssv: String?){
+        db.beginTransaction()
+        try {
+            db.execSQL("update $tableName set status = 0 where mssv = ?", arrayOf(mssv))
+            db.setTransactionSuccessful()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        } finally {
+            db.endTransaction()
+        }
+    }
+
+    fun updateDataStudent(position: Int?, name: String?, mssv: String?, email: String?, phone: String?){
+        db.beginTransaction()
+        try {
+            db.execSQL("update $tableName set name = ?, mssv = ?, email = ?, phone = ? where mssv = ?", arrayOf(name,mssv,email,phone, students[position!!].mssv))
+            db.setTransactionSuccessful()
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        } finally {
+            db.endTransaction()
+        }
+    }
+
+    fun isTableExists(db: SQLiteDatabase?, tableName: String?): Boolean {
+        if (db == null || !db.isOpen || tableName.isNullOrBlank()) return false
+        val cursor = db.rawQuery(
+            "SELECT COUNT(*) FROM sqlite_master WHERE type = ? AND name = ?",
+            arrayOf("table", tableName)
+        )
+        cursor.use {
+            // Luôn có một hàng COUNT(*) => moveToFirst() sẽ trả về true
+            if (it.moveToFirst()) {
+                return it.getInt(0) > 0
+            }
+        }
+        // Trong trường hợp lạ (cursor không có hàng) thì trả về false
+        return false
+    }
+
+
 }
